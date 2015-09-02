81daa118b1d2edee40d9b401244878b8da2537d5
Tom Heslop <t_heslop@hotmail.co.uk>
2015-09-02 20:03:38 +0100

    add styles

--- inst/servevolleyRapp/ui.R
+++ inst/servevolleyRapp/ui.R
@@ -2,12 +2,13 @@ library(shiny)
 
 # Define UI for application that simulates tennis games:sets:matches
 shinyUI(fluidPage(
+    theme = "styles.css",
 
     # Application title
     headerPanel("servevolleyR"),
-    p("This app simulates tennis games, sets, and matches, between two players using the", a("servevolleyR", href = "https://github.com/durtal/servevolleyR"), "package."),
-    p("The sliders under Players A and B are the probability that the player will win a point on their first Serve (top slider), the probability that the player will win a point on their second serve, and the probability that the players first serve will be in.  Changing these sliders wll automatically update the plots underneath, showing the probability that the player will win a game on their serve (and the score of each game, 5-3 is effectively winning after deuce).", style = "font-size: 11px"),
-    p("To simulate sets and matches, then the simulate Button in the middle panel needs to be clicked, simulations are limited to 500 (in order to keep the app relatively speedy), and are based on Player A opening the serving in both the set simulations and match simulations.", style = "font-size: 11px"),
+    h4("This app simulates tennis games, sets, and matches, between two players using the", a("servevolleyR", href = "https://github.com/durtal/servevolleyR"), "package."),
+    h5("The sliders under Players A and B are the probability that the player will win a point on their first Serve (top slider), the probability that the player will win a point on their second serve, and the probability that the players first serve will be in.  Changing these sliders wll automatically update the plots underneath, showing the probability that the player will win a game on their serve (and the score of each game, 5-3 is effectively winning after deuce)."),
+    h5("To simulate sets and matches, then the simulate Button in the middle panel needs to be clicked, simulations are limited to 500 (in order to keep the app relatively speedy), and are based on Player A opening the serving in both the set simulations and match simulations."),
 
     fluidRow(
 
